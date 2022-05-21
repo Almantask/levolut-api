f9d928812717e107c0f868353bf067d82758bea9
AlmantasK <almantusk@gmail.com>
2022-05-21 23:32:17 +0300

    Comment on breaking change

--- Levolut.Api.V2/Models/MoneyExchange.cs
+++ Levolut.Api.V2/Models/MoneyExchange.cs
@@ -2,4 +2,5 @@ using Levolut.Api.V2.Database.Entities;
 
 namespace Levolut.Api.V2.Models;
 
+// Introducing a breaking change.
 public record MoneyExchange(decimal Amount, Currency Currency, string FromCountry);
